6b3eef7bb01ec1791b9f79664cad47288fd02691
Kun-a-Kun <359406732@qq.com>
2021-04-21 22:21:58 +0800

    2021.4.21

---
+++ algorithms/src/chapter3/RedBlackBST.java
@@ -0,0 +1,69 @@
+package chapter3;
+
+public class RedBlackBST<Key extends Comparable<Key>,Value>
+{
+    private Node root;
+    private static final boolean RED=true;
+    private static final boolean BLACK=false;
+
+    private class Node
+    {
+            Key key;
+            Value val;
+            Node left,right;
+            int N;
+            boolean color;
+
+            Node(Key key,Value val,int N,boolean color)
+            {
+                this.key=key;
+                this.val=val;
+                this.N=N;
+                this.color=color;
+            }
+    }
+
+    private boolean isRed(Node x)
+    {
+        if(x==null) {return false;}
+        return x.color==RED;
+    }
+
+    private Node rotateLeft(Node h)
+    {
+
+    }
+
+    private Node rotateRight(Node h)
+    {
+
+    }
+
+    private void flipColors(Node h)
+    {
+
+    }
+
+    private int size()
+    {
+
+    }
+
+    public void put(Key key,Value val)
+    {
+        root=put(root,key,val);
+        root.color=BLACK;
+    }
+
+    private Node put(Node h,Key key,Value val)
+    {
+        if(h==null) {return new Node(key,val,1,RED);}
+        int cmp= key.compareTo(h.key);
+        if(cmp<0)      {h.left=put(h.left,key,val);}
+        else if(cmp>0) {h.right=put(h.right,key,val);}
+        else           {h.val=val;}
+
+        if(isRed(h.right)&&!isRed(h.left))  {h=rotateLeft(h);}
+        if
+    }
+}
